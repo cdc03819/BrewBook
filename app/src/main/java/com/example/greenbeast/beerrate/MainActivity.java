e1b9c403d74bc2d49d5120e8eca05a848d1ef0af
klb05205 <32961305+klb05205@users.noreply.github.com>
2017-10-25 17:46:40 -0400

    Changed oncreate

--- app/src/main/java/com/example/greenbeast/beerrate/MainActivity.java
+++ app/src/main/java/com/example/greenbeast/beerrate/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash);
 
 
     }
