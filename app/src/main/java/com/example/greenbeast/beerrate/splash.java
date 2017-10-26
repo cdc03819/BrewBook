390470fdd67674d0cdca7cc50c6fe0f16db753f8
cdc03819 <33074532+cdc03819@users.noreply.github.com>
2017-10-26 16:48:48 -0400

    Set up the Splash
    splash moves to the main after 2 secs

--- app/src/main/java/com/example/greenbeast/beerrate/splash.java
+++ app/src/main/java/com/example/greenbeast/beerrate/splash.java
@@ -1,13 +1,33 @@
 package com.example.greenbeast.beerrate;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
- * Created by cdc03819 on 10/25/17.
+ * Created by GreenBeast on 10/26/2017.
  */
+public class Splash extends AppCompatActivity {
+    MediaPlayer mpStartmain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
 
-public class splash {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
 
-
+                startActivity(new Intent(Splash.this, MainActivity.class));
+            }
+        };
+        Timer opening = new Timer( );
+        opening.schedule(task,2000);
+    }
 
 }
