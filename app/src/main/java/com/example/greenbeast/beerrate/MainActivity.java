9a1469120206074bca1cb0f4fd93dba0b4e5a8fe
cdc03819 <33074532+cdc03819@users.noreply.github.com>
2017-10-29 23:08:08 -0400

    adding java to link pages

--- app/src/main/java/com/example/greenbeast/beerrate/MainActivity.java
+++ app/src/main/java/com/example/greenbeast/beerrate/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.greenbeast.beerrate;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by GreenBeast on 10/26/2017.
  */
 
 public class MainActivity extends AppCompatActivity {
+   ImageButton trendingBtm,userBtm,locationBtm,settingsBtm;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        trendingBtm = (ImageButton) findViewById(R.id.imageBtmtrending);
+        userBtm = (ImageButton) findViewById(R.id.imageBtmuser);
+        locationBtm = (ImageButton) findViewById(R.id.imageBtmlocation);
+        settingsBtm = (ImageButton) findViewById(R.id.imageBtmSettings);
+
+        trendingBtm.setOnClickListener(btTrending);
     }
+        ImageButton.OnClickListener btTrending= new ImageButton.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, trending.class));
+            }
+
+        };
+
+
 }
---
+++ app/src/main/java/com/example/greenbeast/beerrate/settings.java
@@ -0,0 +1,25 @@
+package com.example.greenbeast.beerrate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+
+/**
+ * Created by GreenBeast on 10/29/2017.
+ */
+
+
+public class settings extends AppCompatActivity {
+    ImageButton trendingBtm,userBtm,locationBtm,settingsBtm;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trending);
+
+        trendingBtm = (ImageButton) findViewById(R.id.imageBtmtrending);
+        userBtm = (ImageButton) findViewById(R.id.imageBtmuser);
+        locationBtm = (ImageButton) findViewById(R.id.imageBtmlocation);
+        settingsBtm = (ImageButton) findViewById(R.id.imageBtmSettings);
+
+
+    }
+}
